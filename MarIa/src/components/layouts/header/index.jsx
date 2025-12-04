d3447928ac37cdcdf2d93bc20515a03f43f8b06f
BiellNonato <gabrielnonato53@gmail.com>
2025-12-04 14:54:35 -0300

    feat:Response header and maps

--- MarIa/src/components/layouts/header/index.jsx
+++ MarIa/src/components/layouts/header/index.jsx
@@ -1,13 +1,35 @@
+import React, { useState } from 'react';
 import avatarMarIa from '../../../assets/images/avatarMarIa.svg';
 import './style.css';
 
 export const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+
+    const navClass = `nav-header ${isMenuOpen ? 'active' : ''}`;
+
     return (
-        
         <header>
             <h1>Mar<span>Ia</span></h1>
 
-            <nav className='nav-header'>
+            {/* BOTÃO TOGGLE: Visível apenas no Mobile */}
+            <button
+                className='menu-toggle'
+                onClick={handleToggleMenu}
+                aria-expanded={isMenuOpen}
+                aria-controls="nav-menu-list"
+            >
+
+                {isMenuOpen ? '✕' : '☰'}
+            </button>
+
+            <nav className={navClass} id="nav-menu-list">
                 <ul>
                     <li><a href="">Home</a></li>
                     <li><a href="">Sobre nós</a></li>
@@ -17,8 +39,6 @@ export const Header = () => {
             </nav>
 
             <img src={avatarMarIa} alt="Avatar personagem MarIa" />
-
-    
         </header>
     );
 };
